dfc9e1555996ee438abd78b81c8b4f3ee6e8ed28
nadirbasalamah <nadir008basalamah@gmail.com>
2023-11-25 07:01:39 +0700

    feat(clear): add clear terminal when continue the game

--- main.go
+++ main.go
@@ -3,7 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
 )
+
+var clear map[string]func()
+
+func init() {
+	clear = make(map[string]func())
+	clear["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
 
 func main() {
 	var choiceIdx int
@@ -42,6 +61,8 @@ func main() {
 			break
 		} else if continuePlaying != "y" && continuePlaying != "n" {
 			fmt.Println("please enter n to quit the game")
+		} else {
+			clearTerminal()
 		}
 	}
 }
@@ -85,5 +106,14 @@ func checkWinner(player, opponent string) string {
 		return "you lose!"
 	} else {
 		return "tie!"
+	}
+}
+
+func clearTerminal() {
+	clearTerminalFunc, ok := clear[runtime.GOOS]
+	if ok {
+		clearTerminalFunc()
+	} else {
+		panic("unsupported platform. clear terminal failed")
 	}
 }
